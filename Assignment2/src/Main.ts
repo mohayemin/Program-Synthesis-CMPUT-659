8358357bff3e5b70c842d9d5beb9a5bddea02316
Mohayemin <mohayeminul.islam@gmail.com>
2021-03-01 18:59:59 +0600

    rename file

--- Assignment2/src/Main.ts
+++ Assignment2/src/Main.ts
@@ -1,4 +1,4 @@
-import { Replace, Str } from "./DSLStarter"
+import { Replace, Str } from "./Node"
 
 let program = new Replace(new Str('a < 4 and a > 0'), new Str('<'), new Str(''))
 console.log(program.toString())
